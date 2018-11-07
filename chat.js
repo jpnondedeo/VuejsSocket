312c6f2e5bd0fc301292816ff87a070f94317bc2
JP <jpnondedeo@gmail.com>
2018-11-07 23:04:04 +0100

    Création du workspace back

---
+++ chat.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+app.use("/style", express.static(__dirname + '/style'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+
+http.listen(3000, () => console.log('Listening on port 3000!'))
