d2275417e78ab038a59ec67a62e97aec682b739e
JP <jpnondedeo@gmail.com>
2018-11-09 18:29:47 +0100

    chatbox avec socketio fonctionnel

--- chat.js
+++ chat.js
@@ -6,4 +6,8 @@ const io = require('socket.io')(http)
 app.use("/style", express.static(__dirname + '/style'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
+io.on('connection', (socket) => {
+    socket.on('message', (msg) => io.emit('message',msg))
+})
+
 http.listen(3000, () => console.log('Listening on port 3000!'))
